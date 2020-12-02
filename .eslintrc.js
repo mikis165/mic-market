eb3c94519edc4f07820f606fd3187cd422cff187
mikis <mikis165@mikiss-MacBook-Pro.local>
2020-12-02 15:27:25 +0100

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "browser": true,
+        "es6": true
+       },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
